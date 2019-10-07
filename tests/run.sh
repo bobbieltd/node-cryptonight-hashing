57cde71c5a06d21ff01b39b73ebc039cfa6d9dd6
Learner <linhdqtg@gmail.com>
2019-10-08 03:08:25 +0700

    Update Rx/0 and add K12 Aeon algo
    From MoneroOcean

--- tests/run.sh
+++ tests/run.sh
@@ -4,11 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 node test.js
+node test_k12.js
 node test_sync-1.js
 node test_sync-2.js
 node test_sync-r.js
 node test_sync-half.js
-node test_sync-wow.js
 node test_sync-msr.js
 node test_sync-xao.js
 node test_sync-rto.js
@@ -23,6 +23,7 @@ node test_sync_heavy.js
 node test_sync_heavy-xhv.js
 node test_sync_heavy-tube.js
 node test_sync_pico.js
+node test_rx0.js
 node test_rx_defyx.js
 node test_rx_wow.js
 node test_rx_loki.js
@@ -31,6 +32,7 @@ node test_ar2_chukwa.js
 node test_ar2_wrkz.js
 
 node test_perf.js
+node test_perf_k12.js
 node test_perf_light.js
 node test_perf_heavy.js
 node test_perf_gpu.js
